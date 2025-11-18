56675e4725331ff284bee1788afde9905ea295d9
Kieran Tribble <kai@macbook-air-8.home>
2025-11-18 22:31:31 +0000

    Added templated constructors to IVPPorblem and BVPProblem to accept any callable

--- include/problem.hpp
+++ include/problem.hpp
@@ -1,27 +1,37 @@
 #pragma once
 #include "types.hpp"
+#include <utility>
 
 namespace diffeq {
 
-// Inital value problem
 struct IVPProblem {
-RHS f;
-Vec y0;
-double t0;
+    RHS f;
+    Vec y0;
+    double t0;
 
-IVPProblem() = default;
+    IVPProblem() = default;
 
-IVPProblem(const RHS& func, const Vec& y_init, double t_start)
-        : f(func), y0(y_init), t0(t_start) {}
+    // Templated constructor to accept any callable
+    template<typename F>
+    IVPProblem(F&& func, const Vec& y_init, double t_start)
+        : f(std::forward<F>(func)), y0(y_init), t0(t_start) {}
 };
 
-// Boundary value problem
 struct BVPProblem {
-RHS f;
-double t0, t_end;
-Vec y0;
-Vec y_end;
+    RHS f;
+    double t0;
+    double t_end;
+    Vec y0;
+    Vec y_end;
+
+    BVPProblem() = default;
+
+    // Templated constructor for any callable
+    template<typename F>
+    BVPProblem(F&& func, double t_start, double t_finish,
+               const Vec& y_init, const Vec& y_final)
+        : f(std::forward<F>(func)), t0(t_start), t_end(t_finish),
+          y0(y_init), y_end(y_final) {}
 };
 
 }
-
